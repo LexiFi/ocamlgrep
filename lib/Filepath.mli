d088cdf02245f4ce54f159a0d8fd3ca2dc8b264c
Martin Jambon <martin@mjambon.com>
2026-06-10 04:00:31 +0000

    Add comments to Filepath

--- lib/Filepath.mli
+++ lib/Filepath.mli
@@ -2,6 +2,14 @@
 
 (* TODO: use the Fpath.t type instead of string *)
 
+(** Same as [Fpath.relativize] but fail with an exception.
+    See also [relativize_dir]. *)
 val relativize : root:string -> string -> string
+
+(** Use this if you know the second argument is a folder even if it
+    doesn't end with a slash. *)
 val relativize_dir : root:string -> string -> string
+
+(** Same as [Fpath.is_prefix]. Beware that this is syntactic
+    i.e. [.] is not considered a prefix of [foo]. *)
 val is_prefix : string -> string -> bool
